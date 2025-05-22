393e0e714f5b52b5a4aa65b77e249542b289c6d2
Almond2107 <almond2100@gmail.com>
2025-05-22 09:21:02 -0400

    updated dotenv

--- main_n.py
+++ main_n.py
@@ -1,5 +1,13 @@
 from sqlalchemy import create_engine, String, Integer
 from sqlalchemy.orm import  sessionmaker, DeclarativeBase, Mapped, mapped_column
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 
 
 class Base(DeclarativeBase):
@@ -36,7 +44,13 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
-DATABASE_URL = "postgresql+psycopg2://postgres:ExploitX2107+try@localhost:5432/classroom_db"
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 
 SessinLocal = sessionmaker(autocommit=False, autoflush=False, bind=DATABASE_URL)
